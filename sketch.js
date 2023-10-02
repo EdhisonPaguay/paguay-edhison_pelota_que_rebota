3d5d589d45f027c4d8b58e97536d6eaa853b45b1
EdhisonPaguay <edhison.paguay@ucuenca.edu.ec>
2023-10-02 15:20:29 -0500

    pelota que reobta corregida

--- sketch.js
+++ sketch.js
@@ -1,31 +1,47 @@
-let posX, posY, diam, rad, esp;
-let piso;
+let posX;
+let posY;
+let vel;
+let diam;
+let rad;
+let esp;
 let margen;
-let velY;
+let piso;
+let acel;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   posX = windowWidth / 2;
-  posY = 100;
+  posY = windowHeight * 0.2;
   rectMode(CENTER);
-  piso = windowHeight - margen - esp / 2;
-  esp = 34;
-  diam = 50;
+  diam = random(10, 50);
   rad = diam / 2;
+  esp = 50;
+  margen = 40;
+  piso = windowHeight - margen - esp / 2;
+  vel = 0;
+  acel = 0.98;
+  //frameRate(10);
 }
 
 function draw() {
-  background(0, 255, 190, 50);
-  noStroke();
+  background(255, 200, 150);
 
-  posY += velY;
+  actualizar();
+
+  noStroke();
+  fill(255, 100, 0);
+  circle(posX, posY, 50);
+  fill(100, 50, 0);
+  rect(windowWidth / 2, windowHeight - margen, windowWidth, esp);
+}
+
+function actualizar() {
+  vel += acel;
+  posY += vel;
 
   if (posY > piso - rad) {
-    velY *= -1;
+    print("ya");
+    vel *= -1;
+    posY += vel;
   }
-
-  fill(255);
-  circle(mouseX, posY, 50);
-
-  fill(255, 0, 0);
-  rect(windowWidth * 0.5, windowWidth - margen, windowWidth * 0.8, esp);
 }
